abd344289bf94250c90ea5da02253367fabb198c
Rory37 <292270@bsd48.org>
2017-11-02 20:41:57 -0700

    Update classvideo.cpp

--- classvideo.cpp
+++ classvideo.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include "classvideo.h"
 #include <cstring>
-using namespace std;
+using namespace std;//needed for strcmp
 
-classvideo :: ~classvideo(){
+classvideo :: ~classvideo(){//destructor of char pointer
   delete publisher;
 }
   
-void classvideo :: setPublisher (char* newPub) {
+void classvideo :: setPublisher (char* newPub) {//sets publisher
   publisher = new char[80];
   strcpy(publisher, newPub);
 }
 
-char* classvideo :: getPublisher () {
+char* classvideo :: getPublisher () {//returns publisher
   return publisher;
 }
 
-void classvideo :: setRating (double newRate) {
+void classvideo :: setRating (double newRate) {//sets rating
   rating = newRate;
 }
 
-double classvideo :: getRating () {
+double classvideo :: getRating () {//returns rating
   return rating;
 }
