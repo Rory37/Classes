7bb922fbb5d983e74186d40da2c248af3a22eb4b
Rory37 <292270@bsd48.org>
2017-11-02 20:38:37 -0700

    Update classparent.cpp

--- classparent.cpp
+++ classparent.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 #include "classparent.h"
-#include <cstring>
+#include <cstring>//for strcmp
 using namespace std;
 
-classparent :: ~classparent() {
+classparent :: ~classparent() {//destructor destroys string pointer
   delete title;
 }
 
-void classparent::setType(int newType) {
+void classparent::setType(int newType) {//sets the type (in terms of int)
   type = newType;
 }
 
-int classparent::getType() {
+int classparent::getType() {//returns the type
   return type;
 }
 
-void classparent::setTitle(char* newTitle) {
+void classparent::setTitle(char* newTitle) { //sets the title
   title = new char[80];
   strcpy(title,newTitle);
 }
 
-char* classparent::getTitle() {
+char* classparent::getTitle() {//returns the title
   return title;
 }
 
-void classparent::setYear(int newYear) {
+void classparent::setYear(int newYear) {//sets the year
   year = newYear;
 }
 
-int classparent::getYear() {
+int classparent::getYear() { // returns the year
   return year;
 }
