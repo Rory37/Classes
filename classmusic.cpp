550bdb1ddbfc431c361efcc831d2883ed34068a1
Rory37 <292270@bsd48.org>
2017-11-02 20:23:00 -0700

    Update classmusic.cpp

--- classmusic.cpp
+++ classmusic.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 #include "classmusic.h"
-#include <cstring>
+#include <cstring> //for strcmp
 using namespace std;
 
-classmusic :: ~classmusic() {
+classmusic :: ~classmusic() {//destructor for char pointer
   delete artist;
   delete publisher;
 }
 
-void classmusic :: setArtist(char* newArt) {
+void classmusic :: setArtist(char* newArt) {//sets the artist
   artist = new char[80];
   strcpy(artist, newArt);
 }
 
-char* classmusic :: getArtist() {
+char* classmusic :: getArtist() { //returns the artist
   return artist;
 }
 
-void classmusic :: setPublish(char* newPub) {
+void classmusic :: setPublish(char* newPub) {//sets the publisher
   publisher = new char[80];
   strcpy(publisher, newPub);
 }
 
-char* classmusic :: getPublish() {
+char* classmusic :: getPublish() {//returns publisher
   return publisher;
 }
 
-void classmusic :: setDur(double newDur) {
+void classmusic :: setDur(double newDur) { //sets duration
   duration = newDur;
 }
 
-double classmusic :: getDur() {
+double classmusic :: getDur() {//returns duration
   return duration;
 }
